714d3ed395f48480c97c928fb47cdf255f92b986
Johha <Johha@itu.dk>
2021-11-07 17:08:01 +0100

    initial navbar

--- clientSide/src/App.js
+++ clientSide/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { NavBar } from "./components/NavBar/Navbar";
 
 function App() {
   return (
     <div className="App">
+    <NavBar/>
+
+    
       <p>hey</p>
+
     </div>
   );
 }

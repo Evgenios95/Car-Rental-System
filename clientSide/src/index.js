f849521a1e7b562b7f2130b3f39eaabe8152c246
Evgenios95 <evgeniospam@gmail.com>
2021-11-16 10:05:52 +0100

    Parse Server initialization

--- clientSide/src/index.js
+++ clientSide/src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import Parse from "parse";
+
+Parse.initialize(
+  process.env.REACT_APP_PARSE_APPLICATION_KEY,
+  process.env.REACT_APP_PARSE_JAVASCRIPT_KEY
+);
+
+Parse.serverURL = "https://parseapi.back4app.com/";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-

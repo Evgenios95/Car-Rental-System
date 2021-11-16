b2d3acaa0a6bf18a5faf161bf502ccff75888b84
Johha <Johha@itu.dk>
2021-11-16 17:09:10 +0100

    added logIn function

--- clientSide/src/components/Input/input.jsx
+++ clientSide/src/components/Input/input.jsx
@@ -1,13 +1,13 @@
 import "./input.css";
 
 const InputComponent = (props) => {
-  const { labelText, inputPlaceholder } = props;
+  const { labelText, inputPlaceholder, changeEvent, onChange } = props;
 
 
   return (
     <div className="inputContainer login-input-item">
       <label htmlFor="">{labelText}</label>
-      <input type="text" placeholder={inputPlaceholder} required />
+      <input type="text" placeholder={inputPlaceholder} onChange={onChange} required/>
     </div>
   );
 };

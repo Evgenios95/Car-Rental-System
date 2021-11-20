acb39f66e49b7daf7cc277d5534f16d132e064a5
Evgenios95 <evgeniospam@gmail.com>
2021-11-20 12:01:55 +0100

    Fixing NavBar component logic & css

--- clientSide/src/components/NavBar/Navbar.jsx
+++ clientSide/src/components/NavBar/Navbar.jsx
@@ -1,32 +1,49 @@
-import {Navbar, Nav, Container, Col, Row} from 'react-bootstrap'
-import "./NavBar.css"
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-export const NavBar = () =>{
-    return (
-      <Navbar className="nav-bar" expand="lg" sticky="top">
-        <Container>
-          <Navbar.Brand href="#home">Scancar</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav"> 
-            <Nav className="ml-auto">         
-                  <Nav.Link id="find" href="find">Find booking</Nav.Link>            
-                  <Nav.Link id="book" href="book">Book</Nav.Link>           
-                  <Nav.Link id="overview" href="overview">Overview</Nav.Link>           
-                  <Nav.Link id="release" href="Release">Release</Nav.Link>           
-                  <Nav.Link id="request" href="request">Request</Nav.Link>         
-                  <Nav.Link  id="logOut" href="logOut" > <i className="fas fa-sign-out-alt"></i> </Nav.Link>          
-            </Nav>
-          </Navbar.Collapse> 
-        </Container>
-      </Navbar>
-    );
-  }
+/**
+ * as={Link} translates the NavLink into a Link from react-router-dom.
+ * removed the !important properties
+ * experimented and added more classes to the elements
+ */
+const NavBar = () => {
+  return (
+    <Navbar className="nav-bar" expand="md" sticky="top">
+      <Container>
+        <Navbar.Brand>Scancar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Nav className="container-fluid">
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="mx-auto text-center w-100"
+          >
+            <Nav.Link className="text-white" as={Link} to="/find-booking">
+              <span>Find booking</span>
+            </Nav.Link>
+            <Nav.Link className="text-white" as={Link} to="/book">
+              <span>Book</span>
+            </Nav.Link>
+            <Nav.Link className="text-white" as={Link} to="/overview">
+              <span>Overview</span>
+            </Nav.Link>
+            <Nav.Link className="text-white" as={Link} to="/release">
+              <span>Release</span>
+            </Nav.Link>
+            <Nav.Link className="text-white" as={Link} to="/request">
+              <span>Request</span>
+            </Nav.Link>
+            <Nav.Link className="text-white ms-auto" as={Link} to="/">
+              <span>
+                <i className="fas fa-sign-out-alt"></i>
+              </span>
+            </Nav.Link>
+          </Navbar.Collapse>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
 
-
-           
-
-   
-       
-    
-   
+export default NavBar;

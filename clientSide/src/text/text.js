cee09fbbaf8c7abdc72e80705a069d4975135f7c
Evgenios95 <evgeniospam@gmail.com>
2021-11-21 08:35:47 +0100

    Cleaning Login, added texts

--- clientSide/src/text/text.js
+++ clientSide/src/text/text.js
@@ -1,6 +1,17 @@
-export const text = {
+export const GeneralText = {
   login: "Log in",
   userText: "Username*",
   pwText: "Password*",
   placeHText: "Type here",
+};
+
+export const CustomerInfoLabels = {
+  title: "Customer info",
+  firstName: "First Name*",
+  lastName: "Last Name*",
+  age: "Age*",
+  address: "Address*",
+  email: "Email*",
+  driversLicensNr: "Driver's License Number*",
+  phoneNumber: "Phone Number*",
 };

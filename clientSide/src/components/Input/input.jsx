10866ffa341ef7308a0400a3c497f2d6e0f9492b
Evgenios95 <evgeniospam@gmail.com>
2021-11-21 09:01:33 +0100

    Input passing type as props

--- clientSide/src/components/Input/input.jsx
+++ clientSide/src/components/Input/input.jsx
@@ -1,13 +1,17 @@
 import "./input.css";
 
 const InputComponent = (props) => {
-  const { labelText, inputPlaceholder, changeEvent, onChange } = props;
-
+  const { labelText, inputPlaceholder, onChange } = props;
 
   return (
     <div className="inputContainer login-input-item">
       <label htmlFor="">{labelText}</label>
-      <input type="text" placeholder={inputPlaceholder} onChange={onChange} required/>
+      <input
+        type="text"
+        placeholder={inputPlaceholder}
+        onChange={onChange}
+        required
+      />
     </div>
   );
 };

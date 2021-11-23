483ddf7b0415fd836ce6800dc56f8eb53766a41d
Evgenios95 <evgeniospam@gmail.com>
2021-11-23 19:38:20 +0100

    nav small fix

--- clientSide/src/components/NavBar/Navbar.jsx
+++ clientSide/src/components/NavBar/Navbar.jsx
@@ -2,6 +2,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { NavigationLabels } from "../../text/text";
 
 /**
  * as={Link} translates the NavLink into a Link from react-router-dom.
@@ -12,7 +13,7 @@ const NavBar = () => {
   return (
     <Navbar className="nav-bar" expand="md" sticky="top">
       <Container>
-        <Navbar.Brand>Scancar</Navbar.Brand>
+        <Navbar.Brand>{NavigationLabels.brand}</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Nav className="container-fluid">
           <Navbar.Collapse
@@ -20,19 +21,19 @@ const NavBar = () => {
             className="mx-auto text-center w-100"
           >
             <Nav.Link className="text-white" as={Link} to="/find-booking">
-              <span>Find booking</span>
+              <span>{NavigationLabels.find}</span>
             </Nav.Link>
             <Nav.Link className="text-white" as={Link} to="/book">
-              <span>Book</span>
+              <span>{NavigationLabels.book}</span>
             </Nav.Link>
             <Nav.Link className="text-white" as={Link} to="/overview">
-              <span>Overview</span>
+              <span>{NavigationLabels.overview}</span>
             </Nav.Link>
             <Nav.Link className="text-white" as={Link} to="/release">
-              <span>Release</span>
+              <span>{NavigationLabels.release}</span>
             </Nav.Link>
             <Nav.Link className="text-white" as={Link} to="/request">
-              <span>Request</span>
+              <span>{NavigationLabels.request}</span>
             </Nav.Link>
             <Nav.Link className="text-white ms-auto" as={Link} to="/login">
               <span>

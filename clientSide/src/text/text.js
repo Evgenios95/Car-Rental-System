799abcc5aa6235ae1e784ff5b5c7526c81c66972
Magnus <Helverskov96@gmail.com>
2021-11-30 10:58:01 +0100

    CarOverview, images and text

--- clientSide/src/text/text.js
+++ clientSide/src/text/text.js
@@ -4,3 +4,68 @@ export const text = {
   pwText: "Password*",
   placeHText: "Type here",
 };
+
+export const CustomerInfoLabels = {
+  title: "Customer info",
+  firstName: "First Name*",
+  lastName: "Last Name*",
+  age: "Age*",
+  address: "Address*",
+  email: "Email*",
+  driversLicensNr: "Driver's License Number*",
+  phoneNumber: "Phone Number*",
+};
+
+export const smallCarType = {
+  carTitle : "Car Group - Small",
+    carModel : "Fiat Punto",
+    carInfo : "3 Passengers & 2 big suitcases"
+  }
+
+export const mediumCarType = {
+  carTitle : "Car Group - Medium",
+  carModel : "VW Polo",
+  carInfo : "4 Passengers & 2 big suitcases and 1 small suitcase"
+}
+
+export const largeCarType = {
+  carTitle : "Car Group - Large",
+  carModel : "Ford Mondeo",
+  carInfo : "4 Passengers & 3 big suitcases and 1 small suitcase"
+}
+
+export const xLargeCarType = {
+  carTitle : "Car Group - X-Large",
+  carModel : "VW Passat",
+  carInfo : "4 Passengers & 3 big suitcases and 1 small suitcase"
+}
+
+export const familyCarType = {
+  carTitle : "Car Group - Family",
+  carModel : "Opel Zafira",
+  carInfo : "5 Passengers & 1 small suitcase"
+}
+
+export const businessCarType = {
+  carTitle : "Car Group - Business",
+  carModel : "Volvo S60",
+  carInfo : "4 Passengers & 3 big suitcases and 1 small suitcase"
+}
+
+export const stationCarType = {
+  carTitle : "Car Group - Station Cars",
+  carModel : "Ford Mondeo",
+  carInfo : "5 Passengers & 5 big suitcases and 1 small suitcase"
+}
+
+export const automaticCarType = {
+  carTitle : "Car Group - Automatic",
+  carModel : "Audi A6 Aut",
+  carInfo : "5 Passengers & 5 big suitcases and 1 small suitcase"
+}
+
+export const smallAutomaticCarType = {
+  carTitle : "Car Group - Small Automatic",
+  carModel : "Seat Toledo Aut",
+  carInfo : "4 Passengers & 3 big suitcases and 1 small suitcase"
+}

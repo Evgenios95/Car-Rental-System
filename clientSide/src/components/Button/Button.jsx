ca82046e3ffe37e715a69fe424b1130617d19289
Evgenios95 <evgeniospam@gmail.com>
2021-12-06 18:30:41 +0100

    Removed styled components

--- clientSide/src/components/Button/Button.jsx
+++ clientSide/src/components/Button/Button.jsx
@@ -1,22 +1,23 @@
 import "../../App.css";
 import "./Button.css";
-import styled from "styled-components";
 
 const Button = (props) => {
   const { btnText, btnColor, btnBgColor, className, onClick, type } = props;
 
-  const Button = styled.button`
-    background-color: ${() => btnBgColor || "#3B3D3D"};
-  `;
-
-  const ButtonText = styled.div`
-    color: ${() => btnColor || "#56d3d3"};
-  `;
+  const colors = {
+    backgroundColor: btnBgColor || "#3B3D3D",
+    color: btnColor || "#56d3d3",
+  };
 
   return (
-    <Button className={`btn-default ${className}`}>
-      <ButtonText onClick={onClick}>{btnText}</ButtonText>
-    </Button>
+    <button
+      className={`btn-default ${className}`}
+      type={type}
+      onClick={onClick}
+      style={{ backgroundColor: colors.backgroundColor }}
+    >
+      <div style={{ color: colors.color }}>{btnText}</div>
+    </button>
   );
 };
 

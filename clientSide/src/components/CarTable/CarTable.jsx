c5956c189f80138d9f5c9a54357b3e41ed341c00
Evgenios95 <evgeniospam@gmail.com>
2021-12-09 19:14:33 +0100

    Cartable: exported parse function, removed comments

--- clientSide/src/components/CarTable/CarTable.jsx
+++ clientSide/src/components/CarTable/CarTable.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./CarTable.css";
 import Parse from "parse";
+import { setCarElements } from "../../parse-functions/carTableFunctions";
 
 const CarTable = () => {
   const [loading, setLoading] = useState(true);
@@ -8,40 +9,8 @@ const CarTable = () => {
   const [error, setError] = useState();
 
   useEffect(() => {
-    setLoading(true);
-    (async () => {
-      const Car = Parse.Object.extend("Car");
-      const query = new Parse.Query(Car);
-      query.include("carGroup");
-      query.include("rentalOffice");
-      query.include("carState");
-      query.include("objectId");
-      const carArray = [];
-
-      try {
-        const results = await query.find();
-        console.log("results", results);
-        for (const object of results) {
-          const carObject = {
-            carGroup: object.get("carGroup").get("name"),
-            model: object.get("model"),
-            licenseNumber: object.get("licenseNumber"),
-            color: object.get("color"),
-            fuelType: object.get("fuelType"),
-            carState: object.get("carState").get("state"),
-            rentalOffice: object.get("rentalOffice").get("officeNumber"),
-          };
-          console.log(carObject.licenseNumber);
-
-          carArray.push(carObject);
-        }
-        setCars(carArray);
-      } catch (error) {
-        console.error("Error while fetching Car", error);
-        setError(error);
-      }
-      setLoading(false);
-    })();
+    await setCarElements(setCars, setError);
+    setLoading(false);
   }, []);
 
   if (loading) {

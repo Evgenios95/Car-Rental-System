b123b3084a96cadf3e9333d48199dcb19ea96603
Johha <Johha@itu.dk>
2021-12-28 15:42:52 +0100

    add if check for non values so they dont render

--- clientSide/src/parse-functions/dropdownFunctions.js
+++ clientSide/src/parse-functions/dropdownFunctions.js
@@ -5,16 +5,20 @@ export const setDropdownElements = async (type, attribute, setElements) => {
   const element = Parse.Object.extend(type);
   const query = new Parse.Query(element);
   //default value for dropdowns
+
   const elementArray = [];
   let filteredArray = (array) => array.filter((v, i) => array.indexOf(v) === i);
   try {
     const results = await query.find();
     for (const object of results) {
       const att = object.get(attribute);
-      elementArray.push(att);
+
+      if (att != "-" && att != 0 && att != "0") {
+        elementArray.push(att);
+      }
     }
     if (typeof elementArray[0] == "number") {
-      elementArray.sort();
+      elementArray.sort((a, b) => a - b);
     }
     setElements(filteredArray(elementArray));
   } catch (error) {

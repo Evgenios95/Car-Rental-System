478863a2b92f54fc28dd7e1a4091f631cf7d2113
Evgenios95 <evgeniospam@gmail.com>
2022-01-16 17:55:56 +0100

    Added filtering by drivers license

--- carRental/src/utils/functions/handleFilteredBookings.js
+++ carRental/src/utils/functions/handleFilteredBookings.js
@@ -1,4 +1,24 @@
-export const handleFilteredBookings = async (
+export const filterBookingsByDriversLicense = async (
+  { target },
+  setfilteredBookings,
+  bookings,
+  setSearchTerm
+) => {
+  //this example is about searching for the first name
+  const searchValue = target.value;
+
+  const filtered = bookings.filter((booking) => {
+    return booking.customerDriversLicense
+      .toString()
+      .toLowerCase()
+      .includes(searchValue.toLowerCase());
+  });
+
+  setSearchTerm(searchValue);
+  await setfilteredBookings(filtered);
+};
+
+export const filterBookingsByLastName = async (
   { target },
   setfilteredBookings,
   bookings,

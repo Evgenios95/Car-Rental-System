8ecc042b3dcb279c0aa315d0dbc7de797fde3c89
Johha <Johha@itu.dk>
2022-01-20 11:05:01 +0100

    better css for returnPage need help for tank label

--- carRental/src/components/CheckBox/CheckBox.jsx
+++ carRental/src/components/CheckBox/CheckBox.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-
+import "./CheckBox.css";
 const Checkbox = ({ label, isSelected, onCheckboxChange }) => (
   <div className="form-check">
-    <label>
-      <input
-        type="checkbox"
-        name={label}
-        checked={isSelected}
-        onChange={onCheckboxChange}
-        className="form-check-input"
-      />
-      {label}
-    </label>
+    <label className="check-label">{label}</label>
+    <input
+      type="checkbox"
+      name={label}
+      checked={isSelected}
+      onChange={onCheckboxChange}
+      className="form-check-input"
+    />
   </div>
 );
 
